80823446c669ab40460a5a0cc398c81212d0b675
Nighthowler713 <anthonyvi713@gmail.com>
2016-09-23 08:55:07 -0700

    Add files via upload

---
+++ Text1.java
@@ -0,0 +1,27 @@
+//save this file as Text1.java
+
+class Text1 {
+
+      public static void (String a[]){
+      
+      
+      
+      i= 7;
+      c = 'A';
+      s = "data";
+      System.out.println(".....................");
+      System.out.println(i);
+      System.out.println(c);
+      System.out.println(s);
+          System.out.println(".......................);
+          //do some math on the variables
+      i + i % 7;
+      ci = (int)c %7;
+      String s = "throw me a bone";
+      System.out.println(".....................");
+      System.out.println(i);
+      System.out.println(ci);
+      
+      )
+      
+}

b2fd218e14070431ad6619643e395c8fa0a19c74
AnthonyIngarra <anthonyvi713@gmail.com>
2016-10-24 08:24:27 -0700

    Add files via upload

---
+++ code1/Asc.java
@@ -0,0 +1,16 @@
+class Asc {
+	public static void main (String a[]){
+		//variables
+		int i;// integer i
+		char c;// character c
+		//for loop
+		for (i = 0; i < 256; i++){
+			c = (char)i;
+			System.out.println(i);
+			System.out.print(" ");
+			System.out.print(c);
+				System.out.print(" ");
+		}
+	
+	}
+}	
---
+++ code1/CWrite.java
@@ -0,0 +1,14 @@
+//
+
+class Cwrite {
+    public static void main (String a[]){
+       int i,j,n ;
+       char c;
+       for (j = 0; j < 20000; j++){
+           for(i = 0; j < 80; j ++){
+               System.out.println(n);
+               n++;
+
+
+     }
+}
